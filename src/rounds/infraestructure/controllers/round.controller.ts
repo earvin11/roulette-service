4f1c9fe06b50d7485b112813be3e1f2653569ec6
Earvin Gomez <earvinsantiago.11@gmail.com>
2025-05-14 12:08:09 -0400

    endRound endpoint

--- src/rounds/infraestructure/controllers/round.controller.ts
+++ src/rounds/infraestructure/controllers/round.controller.ts
@@ -1,15 +1,22 @@
 import { Body, Controller, Put } from '@nestjs/common';
-import { CreateRoundDto } from '../dtos/create-round.dto';
+import { CreateOrEndRoundDto } from '../dtos/create-round.dto';
 import { CreateRoundUseCase } from 'src/rounds/application/create-round.use-case';
+import { EndRoundUseCases } from 'src/rounds/application/end-round.use-case';
 
 @Controller('round')
 export class RoundController {
     constructor(
-        private readonly createRoundUseCases: CreateRoundUseCase
+        private readonly createRoundUseCases: CreateRoundUseCase,
+        private readonly endRoundUseCases: EndRoundUseCases,
     ) {}
 
     @Put('start')
-    async start(@Body() createRoundDto: CreateRoundDto) {
+    async start(@Body() createRoundDto: CreateOrEndRoundDto) {
         return await this.createRoundUseCases.run(createRoundDto);
+    }
+
+    @Put('end')
+    async end(@Body() endRoundDto: CreateOrEndRoundDto) {
+        return await this.endRoundUseCases.run(endRoundDto);
     }
 }
--- src/rounds/infraestructure/dtos/create-round.dto.ts
+++ src/rounds/infraestructure/dtos/create-round.dto.ts
@@ -1,6 +1,6 @@
 import { IsString } from 'class-validator';
 
- export class CreateRoundDto {
+ export class CreateOrEndRoundDto {
     @IsString()
     ID_Ruleta: string;
 

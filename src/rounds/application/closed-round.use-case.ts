a7f7505648bab304823d7bbd454173a46ac75f12
Earvin Gomez <earvinsantiago.11@gmail.com>
2025-05-20 11:20:10 -0400

    delete roundCache in roundClosed

--- src/rounds/application/closed-round.use-case.ts
+++ src/rounds/application/closed-round.use-case.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { RoundUseCases } from './round.use-cases';
+import { RoundCacheUseCases } from './round-cache.use-cases';
 
 @Injectable()
 export class ClosedRoundUseCase {
     constructor(
         private readonly roundUseCases: RoundUseCases,
+        private readonly roundCacheUseCases: RoundCacheUseCases,
     ) {}
     async run(roundUuid: string) {
         const round = await this.roundUseCases.findOneBy({ uuid: roundUuid });
@@ -12,7 +14,9 @@ export class ClosedRoundUseCase {
             error: true,
             message: 'Error round no encontrado',
             roundUuid,
-        }
+        };
+
+        await this.roundCacheUseCases.remove(roundUuid);
 
         return await this.roundUseCases.updateByUuid(roundUuid, {
             open: false
